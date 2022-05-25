41f9b41a1c0b9a885570591be63d381269398cc5
Iouri Volkov <volk.iouri@gmail.com>
2022-05-25 18:27:42 -0400

    Adjust response card css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,15 @@
 const Card = ({ prompt, response }) => {
   return (
     <div className="card">
-      <div className="prompt-data">
-        <div className="card-prompt">Prompt:</div>
-        <div className="card-prompt-prev">{prompt}</div>
-      </div>
-      <div className="response-data">
-        <div className="card-response">Response:</div>
-        <div className="card-reponse-prev">{response}</div>
+      <div className="card-content">
+        <div className="prompt-data">
+          <div className="card-prompt">Prompt:</div>
+          <div className="card-prompt-prev">{prompt}</div>
+        </div>
+        <div className="response-data">
+          <div className="card-response">Response:</div>
+          <div className="card-reponse-prev">{response}</div>
+        </div>
       </div>
     </div>
   );
