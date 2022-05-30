06a0c11c7a76006224177dd913c36fc9479b40d2
Iouri Volkov <volk.iouri@gmail.com>
2022-05-30 16:10:12 -0400

    Add state to prompt input

--- src/components/Prompt.jsx
+++ src/components/Prompt.jsx
@@ -1,23 +1,33 @@
 import { useState } from "react";
 
 const Prompt = () => {
+  const [promptText, setPromptText] = useState("");
+
+  const handlePromptTextChange = (e) => {
+    setPromptText(e.target.value);
+  };
+
   return (
     <div className="prompt">
-      {/* <label className="text-area-label" for="enter-prompt">
+      <form>
+        {/* <label className="text-area-label" for="enter-prompt">
         Enter a prompt to begin:
       </label> */}
-      <textarea
-        rows="15"
-        cols="60"
-        id="enter-prompt"
-        name="prompt"
-        placeholder="Enter your prompt here.."
-      />
-      <div className="buttons">
-        <button type="submit" className="send-prompt-button">
-          submit
-        </button>
-      </div>
+        <textarea
+          rows="15"
+          cols="60"
+          id="enter-prompt"
+          name="prompt"
+          placeholder="Enter your prompt here.."
+          onChange={handlePromptTextChange}
+          value={promptText}
+        />
+        <div className="buttons">
+          <button type="submit" className="send-prompt-button">
+            submit
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
